8e19d8f35af7832f039b6774b8898854c2e5cd8b
m-shvchk <thirdinthequeue@gmail.com>
2022-05-27 20:14:31 +0300

    connected to the DB

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('04 Store API')
+require("dotenv").config();
+// async errors
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect")
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+
+app.use(express.json()); // we don't use it here, just not to forget the syntax
+
+// routes
+
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='api/v1/products'>products route</a>");
+});
+
+// products routes
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connect DB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening at port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
