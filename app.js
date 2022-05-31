6b14ba4ef8919a730ef6990e929edddaf9a5e1c8
m-shvchk <thirdinthequeue@gmail.com>
2022-05-31 07:49:08 +0300

    routes

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const connectDB = require("./db/connect")
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products")
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
 });
 
 // products routes
+app.use('/api/v1/products', productsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
