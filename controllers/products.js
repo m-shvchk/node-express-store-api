28f3380a2cdf55969b9df304d9ef119260b16811
m-shvchk <thirdinthequeue@gmail.com>
2022-06-02 23:27:16 +0300

    implementing sort and select

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,13 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'a'
-  const products = await Product.find({
-    name: {$regex: search, $options: 'i' }
-  })
+  const products = await Product.find({}).sort('name').select('name price')
   res.status(200).json({ products, nbHits: products.length});
 };
 
 const getAllProducts = async (req, res) => {
 
-  const { featured, company, name } = req.query; 
+  const { featured, company, name, sort, fields } = req.query; 
   // here we are querying for featured products, all other queries will be ignored
   const queryObject = {};
   // set the property in the queryObject only if it was set in the request:
@@ -20,8 +17,25 @@ const getAllProducts = async (req, res) => {
 
   if(name) queryObject.name = {$regex: name, $options: 'i' } // using mongobd query oerator - regex
 
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  // console.log(queryObject);
+
+  // Using a method on the Model without await returns an object (a Query object), not the result of a query (we need query object here to chain 'sort'):
+  let result = Product.find(queryObject);
+
+  if(sort){
+    const sortList = sort.split(',').join(' '); // we send request with query params seperated by comma, but 'sort' requires space instead
+    result = result.sort(sortList);
+  } else{
+    result = result.sort('createdAt') // default
+  }
+
+  if(fields){ // fields is a param name made by us to implement the 'select' method 
+    const fieldsList = fields.split(',').join(' '); 
+    result = result.select(fieldsList);
+  }
+
+  const products = await result
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
