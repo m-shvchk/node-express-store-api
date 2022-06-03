e69099047496f8b285ad4b1331f5173a87960c7f
m-shvchk <thirdinthequeue@gmail.com>
2022-06-03 21:03:54 +0300

    pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('name').select('name price')
+  const products = await Product.find({}).sort('name').select('name price').limit(10).skip(1)
   res.status(200).json({ products, nbHits: products.length});
 };
 
@@ -33,6 +33,11 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' '); 
     result = result.select(fieldsList);
   }
+
+  const page = Number(req.query.page) || 1; 
+  const limit = Number(req.query.limit) || 10; // Query.prototype.limit() specifies the maximum number of documents the query will return
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit) // Query.prototype.skip() - how many documents to skip
 
   const products = await result
 
